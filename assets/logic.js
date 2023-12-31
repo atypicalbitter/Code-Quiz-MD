a39c6841a7f048ff07b30ef87fd0b1a4eab3941b
Matt Dudman <mattdudman95@googlemail.com>
2023-12-31 14:43:53 +0000

    5 Questions added with choices and correct answers included. Timer and time limit added as variables.

---
+++ assets/logic.js
@@ -0,0 +1,30 @@
+let quizData = [
+    {
+    question: "What is JavaScript?",
+    choices: ["A programming language", "A coffee brand", "A car model", "An island"],
+    correctAnswer: "A programming language",
+    },
+    {
+    question: "What does HTML stand for?",
+    choices: ["Hyper Text Markup Language", "High-level Text Machine Language", "Hyperlink and Text Markup Language", "Home Tool Markup Language"],
+    correctAnswer: "Hyper Text Markup Language",
+    },
+    {
+    question: "What is the capital of France?",
+    choices: ["London", "Berlin", "Madrid", "Paris"],
+    correctAnswer: "Paris",
+    },
+    {
+    question: "Which programming language is known for building dynamic websites?",
+    choices: ["Java", "Python", "Ruby", "JavaScript"],
+    correctAnswer: "JavaScript",
+    },
+    {
+    question: "What does CSS stand for?",
+    choices: ["Counter Strike: Source", "Corrective Style Sheet", "Computer Style Sheet", "Cascading Style Sheet"],
+    correctAnswer: "Cascading Style Sheet",
+    },];
+
+        let currentQuestionIndex = 0;
+        let timer;
+        let timeLeft = 60; 
