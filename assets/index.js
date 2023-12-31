4e700694e15be541721e00bfe9ce6196d6ebafb9
Matt Dudman <mattdudman95@googlemail.com>
2023-12-31 15:54:45 +0000

    Function to submit a score was added in logic.js and function to start quiz, show questions and hide start screen when questions start has been added in also.

--- assets/index.js
+++ assets/index.js
@@ -2,3 +2,9 @@ const startBtn = document.getElementById("start");
 const submitBtn = document.getElementById("submit");
 startBtn.addEventListener("click", startQuiz);
 submitBtn.addEventListener("click", submitScore);
+
+function startQuiz() {
+    document.getElementById("start-screen").classList.add("hide");
+    document.getElementById("questions").classList.remove("hide");
+    startQuizLogic();
+}
--- assets/logic.js
+++ assets/logic.js
@@ -34,4 +34,6 @@ let quizData = [
             startTimer();
         }
 
-
+        function submitScoreLogic() {
+            submitScore();
+        }
