f5f1967a0a694811c5ad43df465ff01545f00e86
Matt Dudman <mattdudman95@googlemail.com>
2024-01-01 13:58:23 +0000

    Scores now able to be listed, stored and displayed on local storage.

--- assets/index.js
+++ assets/index.js
@@ -11,3 +11,21 @@ function startQuiz() {
 function submitScore() {
     submitScoreLogic();
 }
+
+function loadHighscores() {
+    const highscoresList = document.getElementById("highscores");
+    highscoresList.innerHTML = "";
+  
+    window.onload = function () {
+      loadHighscores();
+    };
+
+    const scores = JSON.parse(localStorage.getItem("scores")) || [];
+
+    scores.sort((a, b) => b.score - a.score);
+    scores.forEach((score, index) => {
+        const listItem = document.createElement("li");
+        listItem.textContent = `${index + 1}. ${score.initials} - ${score.score}`;
+        highscoresList.appendChild(listItem);
+      });
+    }
